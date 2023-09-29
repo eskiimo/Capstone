f1b94d2d65c6ffbfc2e080e146bc5dbab0390533
eskiimo <kareeemgm@gmail.com>
2023-09-29 07:32:49 +0300

    grid messy

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Header() {
-  return <header></header>;
+  return <header>header</header>;
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,19 +1,18 @@
 import React from "react";
+import "./nav.css";
 
 export default function Nav() {
   return (
     <nav>
-      <ul>
-        <li>
-          <img alt="logo" src="/assets/svgs/logo.svg" />
-        </li>
-        <li>
+      <img id="logo" alt="logo" src="/assets/svgs/logo.svg" />
+      <ul className="nav-links">
+        <li className="navlink">
           <a href="/home">Homepage</a>
         </li>
-        <li>
+        <li className="navlink">
           <a href="/about">About Us</a>
         </li>
-        <li>
+        <li className="navlink">
           <a href="/blog">Blog</a>
         </li>
       </ul>
